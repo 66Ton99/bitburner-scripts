396caa7440885be5d630ae69594a7dff019c0e36
Ton Sharp <github@66ton99.org.ua>
2026-06-29 01:51:27 +0300

    Add achievements script

---
+++ achievements.js
@@ -0,0 +1,15 @@
+/** @param {NS} ns **/
+export async function main(ns) {
+    eval("ns.bypass(document)"); // bypass
+    ns.exploit(); // undocumented
+    if (ns.rainbow("noodles")) {} // rainbow
+    window.performance.now = function () { return 0; }; // time compression
+    ns.alterReality(); // Only part of: reality alteration
+    document.getElementById('unclickable').style = "display: block;position: absolute;top: 50%;left: 50%;width: 100px;height: 100px;z-index: 10000;background: red;"; document.getElementById('unclickable').parentNode.addEventListener('click', () => {
+        let unclickableEl = document.getElementById('unclickable');
+        unclickableEl.style = "display: none; visibility: hidden;";
+        // unclickableEl.click();
+    }, true); // unclickable
+
+    Number.prototype.toExponential = function () { return null; }; // prototype tampering
+}
